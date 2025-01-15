241e6e667dd6ccc3ca5fc11f083804ff56dbc284
chrispando <christopherpando2@gmail.com>
2025-01-15 16:39:27 -0600

    "first commit"

---
+++ src/pages/Home.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="mb-20">
+      <h1 className="sm:text-4xl text-2xl font-bold my-6 text-gray-900">
+        Hello, Welcome to AV Knowledge
+      </h1>
+      <p className="mx-auto leading-relaxed text-base mb-4">
+        Lorem ipsum odor amet, consectetuer adipiscing elit. Auctor curabitur
+        venenatis pharetra ligula malesuada nunc donec varius. Neque luctus
+        vivamus fusce maximus duis laoreet diam. Aleo fusce dictumst ipsum ut
+        praesent habitasse. Nascetur felis phasellus vulputate pharetra nisi
+        pharetra phasellus. Dignissim dolor eu consequat maximus dis rhoncus.
+      </p>
+      <p className="mx-auto leading-relaxed text-base mb-4">
+        Lorem ipsum odor amet, consectetuer adipiscing elit. Auctor curabitur
+        venenatis pharetra ligula malesuada nunc donec varius. Neque luctus
+        vivamus fusce maximus duis laoreet diam. Aleo fusce dictumst ipsum ut
+        praesent habitasse. Nascetur felis phasellus vulputate pharetra nisi
+        pharetra phasellus. Dignissim dolor eu consequat maximus dis rhoncus.
+        Lorem ipsum odor amet, consectetuer adipiscing elit. Auctor curabitur
+        venenatis pharetra ligula malesuada nunc donec varius. Neque luctus
+        vivamus fusce maximus duis laoreet diam. Aleo fusce dictumst ipsum ut
+        praesent habitasse. Nascetur felis phasellus vulputate pharetra nisi
+        pharetra phasellus. Dignissim dolor eu consequat maximus dis rhoncus.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
