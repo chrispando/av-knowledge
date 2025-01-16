295a93f32a4c68ea5ce848bb5a0f8e224f91bb1d
chrispando <christopherpando2@gmail.com>
2025-01-15 22:56:37 -0600

    deploying to heroku

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,67 @@
 const express = require("express");
+const { MongoClient, ObjectId } = require("mongodb");
 
 const app = express();
-
 const port = process.env.PORT || 8000;
 
+require("dotenv").config();
+const cors = require("cors");
+app.use(cors());
+
 // Middleware
-//we used to have to install body parser but now it is a built-in middleware
-//function of express.It parses incoming JSON payload
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const withDb = async (operations, res) => {
+  try {
+    const client = await MongoClient.connect(process.env.MONGO_URI);
+    const db = client.db("Knowledge-Base");
+    await operations(db);
+    client.close();
+  } catch (err) {
+    console.error("Error:", err);
+    res
+      .status(500)
+      .json({ message: "Error connecting to db", error: err.message });
+  }
+};
+
+// Route to get all entries
+app.get("/api/articles", (req, res) => {
+  withDb(async (db) => {
+    const articles = await db.collection("Articles").find({}).toArray(); // Adjust the collection name as needed
+    res.status(200).json(articles);
+  }, res);
 });
 
-app.post("/", (req, res) => {
-  res.send(`Hello ${req.body.name}!`);
+app.get("/api/articles/:id", async (req, res) => {
+  const articleId = req.params.id;
+  withDb(async (db) => {
+    const article = await db
+      .collection("Articles")
+      .findOne({ _id: new ObjectId(articleId) });
+    res.status(200).json(article);
+  }, res);
+});
+
+app.post("/api/articles/:id/add-upvote", (req, res) => {
+  const articleId = req.params.id;
+  withDb(async (db) => {
+    const articleInfo = await db
+      .collection("Articles")
+      .findOne({ _id: new ObjectId(articleId) });
+    await db.collection("Articles").updateOne(
+      { _id: new ObjectId(articleId) },
+      {
+        $set: {
+          upVotes: articleInfo.upVotes + 1,
+        },
+      }
+    );
+    const updatedArticleInfo = await db
+      .collection("Articles")
+      .findOne({ _id: new ObjectId(articleId) });
+    res.status(200).json(updatedArticleInfo);
+  }, res);
 });
 
 app.listen(port, () => {
