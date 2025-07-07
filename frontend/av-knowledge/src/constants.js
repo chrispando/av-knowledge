70cd3ca384737f877cdf4c4e3e06a1e6ac84d5e2
chrispando <45864608+chrispando@users.noreply.github.com>
2025-07-07 11:55:24 -0500

    Update constants.js

--- frontend/av-knowledge/src/constants.js
+++ frontend/av-knowledge/src/constants.js
@@ -1,3 +1,1 @@
-export const BASE_URL = "https://stark-ridge-88768-8900bd98165c.herokuapp.com";
-export const ARTICLES_URL = `${BASE_URL}/api/articles`;
-export const USERS_URL = `${BASE_URL}/api/users`;
+
